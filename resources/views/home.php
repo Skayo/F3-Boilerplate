86532a4e6c4fd9e94a21f2033255b09a67c05272
Skayo <jonas@skayo.dev>
2021-03-30 21:21:07 +0200

    Added CSS

--- resources/views/home.php
+++ resources/views/home.php
@@ -5,6 +5,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 		<title>Fat-Free Framework</title>
+
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.min.css" />
 	</head>
 	<body>
 		<h1>Hello World!</h1>
